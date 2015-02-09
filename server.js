bcfbdc29599930d33d2defcc0981cd01e8bf40a4
firrae <firrae@gmail.com>
2015-02-09 16:52:08 -0500

    Added Heroku enviroment variables

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/api/posts', require('./controllers/api/posts'))
 app.use(require('./controllers/static'))
 
-app.listen(80, function() {
-    console.log('Server listening on', 80)
+app.listen(app.get('port'), function() {
+    console.log('Node app is running at localhost:', app.get('port'))
 })
